0a6b320ef69efb152f5ad064364b3bc39ba7b24d
Sai Tat Kelvin Lam <kelvinl@sakurasky.com>
2016-04-30 18:47:40 +1000

    Initial import

---
+++ HipChatStringParser/headers/BridgingHeader.h
@@ -0,0 +1,13 @@
+//
+//  BridgingHeader.h
+//  HipChatStringParser
+//
+//  Created by Sai Tat Lam on 30/04/2016.
+//  Copyright © 2016 Sai Tat Lam. All rights reserved.
+//
+
+#ifndef BridgingHeader_h
+#define BridgingHeader_h
+
+
+#endif /* BridgingHeader_h */
