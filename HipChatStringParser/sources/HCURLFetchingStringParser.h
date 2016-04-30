7cbf25d134b10f5c26269c5eae646d99c3fb8f61
Sai Tat Kelvin Lam <lamsaitat@gmail.com>
2016-05-01 02:37:28 +1000

    Craeted empty class for parser that can fetch url page title

---
+++ HipChatStringParser/sources/HCURLFetchingStringParser.h
@@ -0,0 +1,15 @@
+//
+//  HCURLFetchingStringParser.h
+//  HipChatStringParser
+//
+//  Created by Sai Tat Lam on 1/05/2016.
+//  Copyright © 2016 Sai Tat Lam. All rights reserved.
+//
+
+#import "HCStringParser.h"
+
+/** Adds support for fetching the page title from url.
+ */
+@interface HCURLFetchingStringParser : HCStringParser
+
+@end
---
+++ HipChatStringParser/sources/HCURLFetchingStringParser.m
@@ -0,0 +1,17 @@
+//
+//  HCURLFetchingStringParser.m
+//  HipChatStringParser
+//
+//  Created by Sai Tat Lam on 1/05/2016.
+//  Copyright © 2016 Sai Tat Lam. All rights reserved.
+//
+
+#import "HCURLFetchingStringParser.h"
+
+@implementation HCURLFetchingStringParser
+
+- (void)pageTitleForURL:(NSURL *)url completionBlock:(nullable void(^)(NSString * __nullable, NSError * __nullable))completionBlock {
+    
+}
+
+@end
