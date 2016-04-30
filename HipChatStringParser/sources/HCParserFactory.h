057b269a9bf699a2eee26de62c5fa0bf3476347b
Sai Tat Kelvin Lam <kelvinl@sakurasky.com>
2016-04-30 20:33:16 +1000

    Added stub class files for parser factory

---
+++ HipChatStringParser/sources/HCParserFactory.h
@@ -0,0 +1,13 @@
+//
+//  HCParserFactory.h
+//  HipChatStringParser
+//
+//  Created by Sai Tat Lam on 30/04/2016.
+//  Copyright © 2016 Sai Tat Lam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HCParserFactory : NSObject
+
+@end
---
+++ HipChatStringParser/sources/HCParserFactory.m
@@ -0,0 +1,13 @@
+//
+//  HCParserFactory.m
+//  HipChatStringParser
+//
+//  Created by Sai Tat Lam on 30/04/2016.
+//  Copyright © 2016 Sai Tat Lam. All rights reserved.
+//
+
+#import "HCParserFactory.h"
+
+@implementation HCParserFactory
+
+@end
