0c5fbc4720b04577104befc481773d85861ba33b
Sai Tat Kelvin Lam <kelvinl@sakurasky.com>
2016-04-30 20:48:35 +1000

    Added stub class for string parser.

---
+++ HipChatStringParser/sources/HCStringParser.h
@@ -0,0 +1,14 @@
+//
+//  HCStringParser.h
+//  HipChatStringParser
+//
+//  Created by Sai Tat Lam on 30/04/2016.
+//  Copyright © 2016 Sai Tat Lam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "HCParser.h"
+
+@interface HCStringParser : NSObject<HCParser>
+
+@end
---
+++ HipChatStringParser/sources/HCStringParser.m
@@ -0,0 +1,28 @@
+//
+//  HCStringParser.m
+//  HipChatStringParser
+//
+//  Created by Sai Tat Lam on 30/04/2016.
+//  Copyright © 2016 Sai Tat Lam. All rights reserved.
+//
+
+#import "HCStringParser.h"
+
+@implementation HCStringParser
+
+- (NSArray<NSString *> *)mentionsFromString:(NSString *)sourceString {
+    // TODO: Implement the method.
+    return @[];
+}
+
+- (NSArray<NSString *> *)emoticonsFromString:(NSString *)sourceString {
+    // TODO: Implement the method.
+    return @[];
+}
+
+- (NSArray<NSString *> *)urlLinksFromString:(NSString *)sourceString {
+    // TODO: Implement the method.
+    return @[];
+}
+
+@end
