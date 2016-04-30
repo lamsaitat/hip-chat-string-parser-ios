fbbe80d20dad69a3673f7deba088ad86ef559b62
Sai Tat Kelvin Lam <lamsaitat@gmail.com>
2016-05-01 03:32:08 +1000

    Added more test cases to page title fetching

--- HipChatStringParserTests/HipChatStringParserFetchingUrlLinksTests.swift
+++ HipChatStringParserTests/HipChatStringParserFetchingUrlLinksTests.swift
@@ -42,4 +42,61 @@ class HipChatStringParserFetchingUrlLinksTests: XCTestCase {
         }
     }
     
+    func testPageTitleFetchingWithLongerPageTitle() {
+        let inputString = "http://nshipster.com/xctestcase/"
+        let url = NSURL(string: inputString)
+        let expectedResult = "XCTestCase / XCTestExpectation /  measureBlock() - NSHipster"
+        let expectation = expectationWithDescription("GET \(url)")
+        
+        let task = parser!.pageTitleForURL(
+            url,
+            completionBlock: { (pageTitle: String?, error: NSError?) in
+                
+                XCTAssertTrue(error == nil && pageTitle != nil && pageTitle! == expectedResult)
+                expectation.fulfill()
+            }
+        )
+        
+        waitForExpectationsWithTimeout(task.originalRequest!.timeoutInterval) { error in
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+            }
+            task.cancel()
+        }
+    }
+    
+    
+    // MARK: - Test for Atlassian test cases
+    
+    func testPageTitleFetchingWithAtlassianTestCase3() {
+        
+        let inputString = "http://www.nbcolympics.com/"
+        let url = NSURL(string: inputString)
+        let expectedResult = "2016 Rio Olympic Games | NBC Olympics"
+        let atlassianProvidedExpectedResult = "NBC Olympics | 2014 NBC Olympics in Sochi Russia"
+        let expectation = expectationWithDescription("GET \(url)")
+        
+        let task = parser!.pageTitleForURL(
+            url,
+            completionBlock: { (pageTitle: String?, error: NSError?) in
+                
+                XCTAssertTrue(error == nil && pageTitle != nil && pageTitle! == expectedResult)
+                // As of 2016-05-01 the page has updated it's title from 2014 Sochi Winter Olympics
+                // to 2016 Rio Olympics
+                XCTAssertTrue(error == nil && pageTitle != nil && pageTitle! != atlassianProvidedExpectedResult)
+                expectation.fulfill()
+            }
+        )
+        
+        waitForExpectationsWithTimeout(task.originalRequest!.timeoutInterval) { error in
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+            }
+            task.cancel()
+        }
+    }
+    
+    func testPageTitleFetchingWithAtlassianTestCase4() {
+        
+    }
 }
