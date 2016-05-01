cf9fd28d75013cbf391f00da1b3439a2bf2ad78d
Sai Tat Kelvin Lam <kelvinl@sakurasky.com>
2016-05-02 01:05:00 +1000

    Added test cases for HCLink's json string getter

--- HipChatStringParserTests/HipChatLinkTests.swift
+++ HipChatStringParserTests/HipChatLinkTests.swift
@@ -143,4 +143,74 @@ class HipChatLinkTests: XCTestCase {
         XCTAssertTrue(outputDict[kHCParserDictionaryUrlKey] as? String == "https://www.google.com")
         XCTAssertTrue(NSDictionary(dictionary: link.dictionary).isEqualToDictionary(expectedDict))
     }
+    
+    
+    func testLinkObjectToJsonStringWithValidDictionary1() {
+        let inputDict = [
+            kHCParserDictionaryUrlKey: "https://www.google.com",
+            kHCParserDictionaryTitleKey: "Google"
+        ]
+        
+        let link = HCLink(dictionary: inputDict)
+        
+        XCTAssertTrue(NSDictionary(dictionary: link.dictionary).isEqualToDictionary(inputDict))
+        
+        let resultingJsonString = link.jsonString
+        
+        // Because a dictionary's order is not guaranteed, therefore it is 
+        // inacurate to test that strings are equal.  Instead, cast it back to a 
+        // json object and compare will be much better.
+        do {
+        let recastedDict = try NSJSONSerialization.JSONObjectWithData(resultingJsonString.dataUsingEncoding(NSUTF8StringEncoding)!, options: .AllowFragments)
+            
+            XCTAssertTrue(NSDictionary(dictionary: recastedDict as! [NSObject : AnyObject]).isEqualToDictionary(inputDict))
+        } catch _ {
+            XCTFail("Test failed due to unable to cast string to json object")
+        }
+    }
+    
+    func testLinkObjectToJsonStringWithInvalidValues1() {
+        let inputDict = [
+            kHCParserDictionaryUrlKey: [NSURL(string: "https://www.google.com")!],
+            kHCParserDictionaryTitleKey: "Google"
+        ]
+        let expectedDict = [
+            kHCParserDictionaryTitleKey: "Google"
+        ]
+        let link = HCLink(dictionary: inputDict)
+        
+        XCTAssertTrue(NSDictionary(dictionary: link.dictionary).isEqualToDictionary(expectedDict))
+        
+        let resultingJsonString = link.jsonString
+        do {
+            let recastedDict = try NSJSONSerialization.JSONObjectWithData(resultingJsonString.dataUsingEncoding(NSUTF8StringEncoding)!, options: .AllowFragments)
+            
+            XCTAssertTrue(NSDictionary(dictionary: recastedDict as! [NSObject : AnyObject]).isEqualToDictionary(expectedDict))
+        } catch _ {
+            XCTFail("Test failed due to unable to cast string to json object")
+        }
+    }
+    
+    func testLinkObjectToJsonStringWithInvalidValues2() {
+        let inputDict = [
+            kHCParserDictionaryUrlKey: NSURL(string: "https://www.google.com")!,
+            kHCParserDictionaryTitleKey: ["Google"]
+        ]
+        let expectedDict = [
+            kHCParserDictionaryUrlKey: "https://www.google.com"
+        ]
+        let link = HCLink(dictionary: inputDict)
+        
+        XCTAssertTrue(NSDictionary(dictionary: link.dictionary).isEqualToDictionary(expectedDict))
+        
+        let resultingJsonString = link.jsonString
+        do {
+            let recastedDict = try NSJSONSerialization.JSONObjectWithData(resultingJsonString.dataUsingEncoding(NSUTF8StringEncoding)!, options: .AllowFragments)
+            
+            XCTAssertTrue(NSDictionary(dictionary: recastedDict as! [NSObject : AnyObject]).isEqualToDictionary(expectedDict))
+        } catch _ {
+            XCTFail("Test failed due to unable to cast string to json object")
+        }
+    }
+
 }
