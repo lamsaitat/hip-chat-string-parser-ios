2f473e7a9ca4ce16406221191dbcc3117f34ea8e
Sai Tat Kelvin Lam <kelvinl@sakurasky.com>
2016-05-02 03:14:49 +1000

    Added basic properties to Message VC

--- HipChatStringParser/MessageViewController.swift
+++ HipChatStringParser/MessageViewController.swift
@@ -9,27 +9,18 @@
 import UIKit
 
 class MessageViewController: UIViewController {
-
+    
+    static let identifier = "MessageViewController"
+    
+    @IBOutlet weak var contentTextView: UITextView!
+    var message: HCMessage?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func reloadButtonTouchUpInside(sender: AnyObject?) {
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
