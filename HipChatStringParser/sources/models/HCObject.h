8fddae9be16ac0dbf9aa8515a42139daf123eb1b
Sai Tat Kelvin Lam <kelvinl@sakurasky.com>
2016-05-01 22:37:40 +1000

    Added protocol definition of HCObject

--- HipChatStringParser/sources/models/HCObject.h
+++ HipChatStringParser/sources/models/HCObject.h
@@ -9,5 +9,16 @@
 #ifndef HCObject_h
 #define HCObject_h
 
+#import <Foundation/Foundation.h>
+
+@protocol HCObject <NSObject>
+
+- (instancetype)initWithDictionary:(NSDictionary *)sourceDictionary;
+
+/** Converts conforming class's properties into a dictionary;
+ */
+@property(readonly, copy) NSDictionary *dictionary;
+
+@end
 
 #endif /* HCObject_h */
