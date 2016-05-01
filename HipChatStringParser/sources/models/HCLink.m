58119972fb20696fe9611504dbcb031c05eb082a
Sai Tat Kelvin Lam <kelvinl@sakurasky.com>
2016-05-01 22:38:22 +1000

    Implemented initWithDictionary and dictionary getter for HCLink object

--- HipChatStringParser/sources/models/HCLink.m
+++ HipChatStringParser/sources/models/HCLink.m
@@ -7,6 +7,7 @@
 //
 
 #import "HCLink.h"
+#import "HCConstants.h"
 
 @implementation HCLink
 
@@ -14,6 +15,17 @@
     self = [super init];
     
     if (self) {
+        if (sourceDictionary) {
+            if ([sourceDictionary[kHCParserDictionaryUrlKey] isKindOfClass:[NSURL class]]) {
+                self.url = sourceDictionary[kHCParserDictionaryUrlKey];
+            } else if ([sourceDictionary[kHCParserDictionaryUrlKey] isKindOfClass:[NSString class]]) {
+                self.url = [NSURL URLWithString:sourceDictionary[kHCParserDictionaryUrlKey]];
+            }
+            
+            if ([sourceDictionary[kHCParserDictionaryTitleKey] isKindOfClass:[NSString class]]) {
+                self.title = sourceDictionary[kHCParserDictionaryTitleKey];
+            }
+        }
     }
     
     return self;
@@ -21,6 +33,13 @@
 
 - (NSDictionary *)dictionary {
     NSMutableDictionary *dict = [NSMutableDictionary dictionary];
+    
+    if (self.url) {
+        dict[kHCParserDictionaryUrlKey] = self.url.absoluteString;
+    }
+    if (self.title) {
+        dict[kHCParserDictionaryTitleKey] = self.title;
+    }
     
     return [NSDictionary dictionaryWithDictionary:dict];  // Lock the dictionary to be immutable.
 }
